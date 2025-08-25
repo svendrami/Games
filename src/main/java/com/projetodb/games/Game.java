e1eabb5f0a60252c850f4d78eb3aeb9873cb0d99
Shirlei Vendrami <shirlei.vendrami@gmail.com>
2025-08-25 00:33:45 -0300

    game seed

--- src/main/java/com/projetodb/games/Game.java
+++ src/main/java/com/projetodb/games/Game.java
@@ -17,18 +17,22 @@ public class Game {
   @Column(name = "game_year")
   private Integer year;
   private String genre;
-  private String platform;
+  private String platforms;
+  private Double score;
   private String imgUrl;
+  @Column(columnDefinition = "TEXT")
   private String shortDescription;
+  @Column(columnDefinition = "TEXT")
   private String longDescription;
 
-  public Game(long id, String title, Integer year, String genre, String platform, String imgUrl,
+  public Game(long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
       String shortDescription, String longDescription) { // criando o construtor
     this.id = id;
     this.title = title;
     this.year = year;
     this.genre = genre;
-    this.platform = platform;
+    this.platforms = platforms;
+    this.score = score;
     this.imgUrl = imgUrl;
     this.shortDescription = shortDescription;
     this.longDescription = longDescription;
@@ -66,12 +70,16 @@ public class Game {
     this.genre = genre;
   }
 
-  public String getPlatform() {
-    return platform;
+  public String getPlatforms() {
+    return platforms;
   }
 
-  public void setPlatform(String platform) {
-    this.platform = platform;
+  public void setPlatforms(String platforms) {
+    this.platforms = platforms;
+  }
+
+  public void setscore(Double score) {
+    this.score = score;
   }
 
   public String getImgUrl() {
@@ -121,6 +129,14 @@ public class Game {
     } else if (!id.equals(other.id))
       return false;
     return true;
+  }
+
+  public Double getScore() {
+    return score;
+  }
+
+  public void setScore(Double score) {
+    this.score = score;
   }
 
 }
